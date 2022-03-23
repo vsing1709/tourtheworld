944cbe771dc113de0d60660f4f9b8dae0447f8d3
vsing1709 <vijay.singh091992@gmail.com>
2022-03-23 17:40:17 +0530

    first commit
    first commit

---
+++ TourTheWorld/src/main/java/com/example/demo/TourTheWorldApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourTheWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourTheWorldApplication.class, args);
+		System.out.println("to check github connectivity");
+	}
+
+}
